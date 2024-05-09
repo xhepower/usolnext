f3e80e6ff50715e2eafc703e2fcaf45359978f2b
Mona Lisa <johndoe@example.com>
2024-05-08 23:33:59 -0600

    images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "http://64.23.214.163:3008"], // Agrega 'localhost' como dominio permitido para las imágenes
+    remotePatterns: [
+      {
+        hostname: "swiperjs.com",
+      },
+      {
+        hostname: "http://64.23.214.163:3008",
+      },
+    ],
   },
 };
 
